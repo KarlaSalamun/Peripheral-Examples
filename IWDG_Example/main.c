a9c7c51108a84d0561dca351eef2ca99701a8e6b
KarlaSalamun <33004569+KarlaSalamun@users.noreply.github.com>
2017-11-14 12:51:20 +0100

    Add files via upload

---
+++ IWDG_Example/main.c
@@ -0,0 +1,178 @@
+#include "main.h"
+
+EXTI_InitTypeDef   EXTI_InitStructure;
+
+static __IO uint32_t uwLsiFreq = 0;
+
+__IO uint32_t uwTimingDelay = 0;
+__IO uint32_t uwCaptureNumber = 0;
+__IO uint32_t uwPeriodValue = 0;
+
+void Delay(__IO uint32_t nTime);
+static uint32_t GetLSIFrequency(void);
+
+int main(void)
+{
+  STM_EVAL_LEDInit(LED3);
+  STM_EVAL_LEDInit(LED4);
+  STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_EXTI);
+
+  if (SysTick_Config(SystemCoreClock / 1000))
+  { 
+     while (1);
+  }
+
+  if (RCC_GetFlagStatus(RCC_FLAG_IWDGRST) != RESET)
+  {
+		STM_EVAL_LEDOn(LED3);
+
+    RCC_ClearFlag();
+  }
+  else
+  {
+		STM_EVAL_LEDOff(LED3);
+  }
+ 
+	uwLsiFreq = GetLSIFrequency();
+   
+	IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
+
+	IWDG_SetPrescaler(IWDG_Prescaler_32);
+ 
+  IWDG_SetReload(uwLsiFreq/32);
+
+	IWDG_ReloadCounter();
+
+  IWDG_Enable();
+
+  while (1)
+  {
+    
+    STM_EVAL_LEDToggle(LED4);
+    
+    Delay(900);
+
+    IWDG_ReloadCounter();  
+  }
+}
+
+static uint32_t GetLSIFrequency(void)
+{
+  NVIC_InitTypeDef   NVIC_InitStructure;
+  TIM_ICInitTypeDef  TIM_ICInitStructure;
+  RCC_ClocksTypeDef  RCC_ClockFreq;
+
+  /* Enable the LSI oscillator ************************************************/
+  RCC_LSICmd(ENABLE);
+  
+  /* Wait till LSI is ready */
+  while (RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET)
+  {
+  }
+
+  /* TIM5 configuration *******************************************************/ 
+  /* Enable TIM5 clock */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+  
+  /* Connect internally the TIM5_CH4 Input Capture to the LSI clock output */
+  TIM_RemapConfig(TIM5, TIM5_LSI);
+
+  /* Configure TIM5 prescaler */
+  TIM_PrescalerConfig(TIM5, 0, TIM_PSCReloadMode_Immediate);
+  
+  /* TIM5 configuration: Input Capture mode ---------------------
+     The LSI oscillator is connected to TIM5 CH4
+     The Rising edge is used as active edge,
+     The TIM5 CCR4 is used to compute the frequency value 
+  ------------------------------------------------------------ */
+  TIM_ICInitStructure.TIM_Channel = TIM_Channel_4;
+  TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+  TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+  TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV8;
+  TIM_ICInitStructure.TIM_ICFilter = 0;
+  TIM_ICInit(TIM5, &TIM_ICInitStructure);
+  
+  /* Enable TIM5 Interrupt channel */
+  NVIC_InitStructure.NVIC_IRQChannel = TIM5_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  /* Enable TIM5 counter */
+  TIM_Cmd(TIM5, ENABLE);
+
+  /* Reset the flags */
+  TIM5->SR = 0;
+    
+  /* Enable the CC4 Interrupt Request */  
+  TIM_ITConfig(TIM5, TIM_IT_CC4, ENABLE);
+
+
+  /* Wait until the TIM5 get 2 LSI edges (refer to TIM5_IRQHandler() in 
+    stm32f4xx_it.c file) ******************************************************/
+  while(uwCaptureNumber != 2)
+  {
+  }
+  /* Deinitialize the TIM5 peripheral registers to their default reset values */
+  TIM_DeInit(TIM5);
+
+
+  /* Compute the LSI frequency, depending on TIM5 input clock frequency (PCLK1)*/
+  /* Get SYSCLK, HCLK and PCLKx frequency */
+  RCC_GetClocksFreq(&RCC_ClockFreq);
+
+  /* Get PCLK1 prescaler */
+  if ((RCC->CFGR & RCC_CFGR_PPRE1) == 0)
+  { 
+    /* PCLK1 prescaler equal to 1 => TIMCLK = PCLK1 */
+    return ((RCC_ClockFreq.PCLK1_Frequency / uwPeriodValue) * 8);
+  }
+  else
+  { /* PCLK1 prescaler different from 1 => TIMCLK = 2 * PCLK1 */
+    return (((2 * RCC_ClockFreq.PCLK1_Frequency) / uwPeriodValue) * 8) ;
+  }
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in milliseconds.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{ 
+  uwTimingDelay = nTime;
+
+  while(uwTimingDelay != 0);
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
